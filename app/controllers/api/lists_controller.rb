bc3bffe50d4fea96c20ad2b5cdbb280d176cc111
kyleashcraft <ashcraft.k@husky.neu.edu>
2018-08-01 16:22:55 -0700

    delete method added for users and lists

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -7,8 +7,18 @@ class Api::ListsController < ApiController
       render json: list
     else
       render json: { errors: list.errors.full_messages }, status: :unprocessable_entity
-    end 
+    end
   end
+
+  def destroy
+    begin
+      list = List.find(params[:id])
+      list.destroy
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
+    end
+  end 
 
   private
   def list_params
