ff93a6b422deeeffbb95647f51f2c70995f8bced
kyleashcraft <ashcraft.k@husky.neu.edu>
2018-08-01 17:51:22 -0700

    update lists and items, completion attribute added to items

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -18,6 +18,15 @@ class Api::ListsController < ApiController
     rescue ActiveRecord::RecordNotFound
       render :json => {}, :status => :not_found
     end
+  end
+
+  def update
+    list = List.find(params[:id])
+    if list.update(list_params)
+      render json: list
+    else
+      render json: { errors: list.errors.full_messages }, status: :unprocessable_entity
+    end
   end 
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180727223303) do
+ActiveRecord::Schema.define(version: 20180802003528) do
 
   create_table "items", force: :cascade do |t|
     t.string "list"
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed"
     t.index ["list"], name: "index_items_on_list"
   end
 
